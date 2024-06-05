7103b64963c75665597f438c13fc0594b1cc0c90
infiniteTsukuyomi <19579887+Tsukuyomi-infinite@users.noreply.github.com>
2024-06-06 03:36:55 +0530

    Add files via upload

--- read_csvfile.py
+++ read_csvfile.py
@@ -7,8 +7,10 @@ df = pd.read_csv('exportSecurityGroupsToCsv.csv')
 def generate_tfvars(df, output_file):
     with open(output_file, 'w') as f:
         for _, row in df.iterrows():
-            f.write(f'group_name_{row["id"]} = "{row["group_name"]}"\n')
-            f.write(f'description_{row["id"]} = "{row["description"]}"\n')
+            # print (f'row-->>> {row}')
+            # id, grp_name, descrp, grp_id
+            f.write(f'GroupId_{row["id"]} = "{row["GroupId"]}"\n')
+            f.write(f'Description_{row["id"]} = "{row["Description"]}"\n')
 
 # Generate the .tfvars file
 generate_tfvars(df, 'security_groups.tfvars')
